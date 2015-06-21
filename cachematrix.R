f5c86d3389f1b432c03d1c385f5e533c6818192d
--replace-all <datacience001@users.noreply.github.com>
2015-06-21 11:54:14 -0700

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,13 +2,15 @@
 ## functions do
 ## makeCacheMatrix function creates a special matrix and sets the inverse of matrix in an environment variable for caching.
 ## cacheSolve function gets the inverse of a matrix from cache if exists otherwise calculates the inverse and passes the inverse of amatrix to makeChacheMatrix to store it in the cache.
-## Order of Execution is - first execute makeCacheMatrix() and assign return value to x,m<- matrix(c(-1,-2,1,1),2,2) x<- makeCacheMatrix(m) 
-## pass x to cacheSolve function - cacheSolve(x)
+
 
 
 ## Write a short comment describing this function
 ## Below function takes a matrxi variable , set resets the matrix from previous values. get gives the matrix from 
 ## envronment . setInvMatrix sets inverse matrix in cache(Environment Variable)
+## Order of Execution is - first execute makeCacheMatrix() and assign return value to x,m<- matrix(c(-1,-2,1,1),2,2) x<- makeCacheMatrix(m) 
+## pass x to cacheSolve function - cacheSolve(x)
+
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -28,6 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 ## below fucntion takes matrix x from the environment , x gets value by the output from above function.
 ## variable inv stores inverse matrix either from cache or by calculating the value
+## pass x calculated by using above function and pass it to  cacheSolve function like cacheSolve(x)
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
